8b8cb29805ee828b3b339423bc06518ded1c984b
jav <javierlopeztl1@gmail.com>
2024-06-13 16:00:00 +0200

    C++ (Verano): Ejercicios de procesamiento de texto - Manipulación de la clase string y funciones de cstring

---
+++ 2p11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+int main(){
+
+	
+		int a,c;
+		char[45];
+		cin.getline(c,45);
+		for(a = 0; c[a]; a++)
+			if((a%2)==0)
+			cout<<a<<endl;
+	
+	system("pause");
+	return 0;
+}
