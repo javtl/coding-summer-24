c369059d24013386ae5699280e904342113b97da
jav <javierlopeztl1@gmail.com>
2024-09-06 18:00:00 +0200

    JS Project: Todo List - Manipulación del DOM y persistencia básica de tareas

---
+++ js/4-ToDoList/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-text"); // Corrige el ID aquí
+const listContainer = document.getElementById("list-container"); // Corrige el ID aquí
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Escribe una nueva tarea");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00D7"; // Corrige el valor del span aquí
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
